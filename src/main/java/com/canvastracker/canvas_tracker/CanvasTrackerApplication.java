f49f6ebac8121fe41dcca8305163612467aa1021
Mushfiqur Rahman <mr4201205@gmail.com>
2026-05-03 19:20:16 -0400

    initial project setup with database connection

---
+++ src/main/java/com/canvastracker/canvas_tracker/CanvasTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.canvastracker.canvas_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanvasTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanvasTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/canvastracker/canvas_tracker/CanvasTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canvastracker.canvas_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanvasTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
