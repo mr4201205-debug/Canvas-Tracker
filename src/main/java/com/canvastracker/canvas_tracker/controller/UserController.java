a714fa62045f3af40ed769f866f707218d8023af
Mushfiqur Rahman <mr4201205@gmail.com>
2026-05-05 14:42:33 -0400

    add UserService and UserController

---
+++ src/main/java/com/canvastracker/canvas_tracker/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.canvastracker.canvas_tracker.controller;
+
+import com.canvastracker.canvas_tracker.model.User;
+import com.canvastracker.canvas_tracker.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        return userService.saveUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+}
