3cd7ac3d3ab51a9eeb521298eb641f5820ebdbd9
Mushfiqur Rahman <mr4201205@gmail.com>
2026-05-08 14:48:48 -0400

    add Assignment entity with Relationship to user

---
+++ src/main/java/com/canvastracker/canvas_tracker/model/Assignment.java
@@ -0,0 +1,43 @@
+package com.canvastracker.canvas_tracker.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "assignments")
+
+public class Assignment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String courseName;
+    private LocalDateTime dueDate;
+    private Double points;
+    private Double gradeWeight;
+    private boolean submitted;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Long getId() {return id;}
+    public String getTitle() {return title;}
+    public String getCourseName() {return courseName;}
+    public LocalDateTime getDueDate() {return dueDate;}
+    public Double getPoints() { return points; }
+    public Double getGradeWeight() { return gradeWeight; }
+    public boolean isSubmitted() { return submitted; }
+    public User getUser() { return user; }
+
+    public void setId(Long id) { this.id = id; }
+    public void setTitle(String title) { this.title = title; }
+    public void setCourseName(String courseName) { this.courseName = courseName; }
+    public void setDueDate(LocalDateTime dueDate) { this.dueDate = dueDate; }
+    public void setPoints(Double points) { this.points = points; }
+    public void setGradeWeight(Double gradeWeight) { this.gradeWeight = gradeWeight; }
+    public void setSubmitted(boolean submitted) { this.submitted = submitted; }
+    public void setUser(User user) { this.user = user; }
+
+}
